bb26583083f2b9362da210b6e56d70a188275870
shashank01234 <spandey21217@gmail.com>
2026-07-27 00:50:29 +0530

    refactor: use lombok annotations for getters/setters in User entity

--- src/main/java/com/sp/recall/model/User.java
+++ src/main/java/com/sp/recall/model/User.java
@@ -1,7 +1,11 @@
 package com.sp.recall.model;
 
+import lombok.*;
 import jakarta.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -15,25 +19,4 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
